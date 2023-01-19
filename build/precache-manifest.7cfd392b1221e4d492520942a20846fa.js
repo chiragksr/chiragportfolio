5761012ffbc67fcfe4e86e7d16851ecf9b7c5579
chiragksr <70831417+chiragksr@users.noreply.github.com>
2023-01-20 04:08:26 +0530

    Add files via upload

---
+++ build/precache-manifest.7cfd392b1221e4d492520942a20846fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e02e8701021decedc6433df0a9ad146",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c318a2c26a1dbb5fa264",
+    "url": "/portfolio/static/css/2.3aee7458.chunk.css"
+  },
+  {
+    "revision": "6ac49396519d2ea4960f",
+    "url": "/portfolio/static/css/main.e8901382.chunk.css"
+  },
+  {
+    "revision": "c318a2c26a1dbb5fa264",
+    "url": "/portfolio/static/js/2.a04ae5a1.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.a04ae5a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ac49396519d2ea4960f",
+    "url": "/portfolio/static/js/main.3c4afb8e.chunk.js"
+  },
+  {
+    "revision": "58d18144e62526203a32",
+    "url": "/portfolio/static/js/runtime-main.770b1f5e.js"
+  }
+]);
